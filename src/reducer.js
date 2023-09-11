57781113fb13ca227ede553c2eac1071626a387c
Zach <zachlandis91@gmail.com>
2023-09-11 14:04:59 -0600

    commit tree 80264b070cef74b638afd902192f535cc07bddf0

--- src/reducer.js
+++ src/reducer.js
@@ -1,1 +1,29 @@
-// add code snippets from README
+let state;
+
+function reducer(state = { count: 0 }, action) {
+    switch (action.type) {
+        case "counter/increment":
+            return { count: state.count + 1 };
+        default:
+            return state;
+    }
+}
+
+function render() {
+    const container = document.getElementById("container");
+    container.textContent = state.count;
+  }
+  
+
+function dispatch(action) {
+    state = reducer(state, action);
+    render();
+}
+
+dispatch({type: '@@init'})
+
+const button = document.getElementById("button")
+
+button.addEventListener("click", () => {
+    dispatch({type: "counter/increment"});
+});
